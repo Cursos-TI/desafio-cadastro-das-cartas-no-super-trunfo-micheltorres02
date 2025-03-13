7dfda1962c0428a14803e197ee1f0f8b33394245
micheltorres02 <micheltorres0065@hotmail.com>
2025-03-13 04:58:50 +0000

    pausa

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,18 +1,46 @@
 #include <stdio.h>
 
     int main() {
-        printf("DESAFIO SUPER TRUNFO!\n");
+//primeira carta        
+        char estado[30] = "pernambuco";
+        char cdg_carta[10] = "a01";
+        char cidade[30] = "recife";
+        int população = 2587150;
+        double area_da_cidade = 25547502;
+        double pib = 58000000;
+        int pontos_turisticos = 50;
+
+//segunda carta
+        char estado[25] = "ceara";
+        char cdg_carta[10] = "b01";
+        char cidade[25] = "fortaleza";
+        int população = 2581100;
+        double area_da_cidade = 20547805;
+        double pib = 56958000;
+        int pontos_turisticos = 43
+
+        printf("desafio super trunfo");
         
-        
 
 
 
 
 
-
-
-
-
-
-
-    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }    
